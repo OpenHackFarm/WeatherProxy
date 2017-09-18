d47089ae8d543f57050cb6577727787a4ea84ec9
yan <a0726h77@gmail.com>
2017-09-19 07:36:27 +0800

    Add argument

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import argparse
+
 from WeatherProxy import WeatherProxy
 
 if __name__ == '__main__':
-    w = WeatherProxy('CWB_OA')
+    parser = argparse.ArgumentParser()
 
-    print w.get_realtime(57)
+    parser.add_argument('--backend', type=str)
+    parser.add_argument('--id', type=str)
+
+    args = parser.parse_args()
+
+    w = WeatherProxy(args.backend)
+
+    print w.get_realtime(args.id)
