48f9339713a4797d2812f62ddb7670fbe9dca0fe
yan <a0726h77@gmail.com>
2017-09-29 13:39:24 +0800

    CWB_OA: get town ship

--- backend/CWB_OA.py
+++ backend/CWB_OA.py
@@ -23,6 +23,10 @@ class CWB_OA:
         "specials": "specials"
     }
 
+    township_column_map = {
+        "name": "township"
+    }
+
     def get_realtime(self, id):
         r = requests.get('https://works.ioa.tw/weather/api/weathers/%s.json' % str(id))
 
@@ -31,7 +35,16 @@ class CWB_OA:
     def get_forecast(self):
         pass
 
+    def get_town(self, id):
+        return [_ for i, _ in enumerate(self.get_towns()) if _['id'] == str(id)][0]
+
     def get_towns(self):
         r = requests.get('https://raw.githubusercontent.com/OpenHackFarm/works.ioa.tw/master/towns.json')
 
-        return r.json()
+        # return sorted(r.json(), key=lambda k: int(k['id']))
+
+        towns = []
+        for town in sorted(r.json(), key=lambda k: int(k['id'])):
+            towns.append(remap_dict_columns(town, self.township_column_map))
+
+        return towns
