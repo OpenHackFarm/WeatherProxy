c01df079f768920a10f4122e3341073a75a06b02
yan <a0726h77@gmail.com>
2017-10-01 22:21:58 +0800

    Add more forecast value

--- backend/CWB.py
+++ backend/CWB.py
@@ -26,8 +26,10 @@ class CWB:
     forecast_column_map = {
         "WeatherDescription": "description",
         "RH": "humidity",
+        "T": "temperature_c",
         "MaxT": "max_temperature_c",
-        "MinT": "min_temperature_c"
+        "MinT": "min_temperature_c",
+        "Wx": "condition"
     }
 
     @property
@@ -39,16 +41,32 @@ class CWB:
         forecast_list = []
 
         json_object = get_data_from_cwb(dataset_id, AUTH_KEY, {})
+        # return json_object
 
         for i in json_object['records']['locations'][0]['location'][int(town_index)]['weatherElement']:
-            if i['elementName'] in ['MinT', 'MaxT', 'RH', 'PoP', 'WeatherDescription']:
+            if i['elementName'] in ['MinT', 'MaxT', 'RH', 'PoP', 'WeatherDescription', 'Wx', 'Wind', 'T', 'UVI']:
                 for j in i['time']:
-                    if j['startTime'] + '-' + j['endTime'] not in forecast_dict:
-                        forecast_dict[j['startTime'] + '-' + j['endTime']] = {}
-                        forecast_dict[j['startTime'] + '-' + j['endTime']]['start_time'] = j['startTime']
-                        forecast_dict[j['startTime'] + '-' + j['endTime']]['end_time'] = j['endTime']
+                    new_time = j['startTime'] + '-' + j['endTime']
+                    if new_time not in forecast_dict:
+                        forecast_dict[new_time] = {}
+                        forecast_dict[new_time]['start_time'] = j['startTime']
+                        forecast_dict[new_time]['end_time'] = j['endTime']
 
-                    forecast_dict[j['startTime'] + '-' + j['endTime']][i['elementName']] = j['elementValue']
+                    if i['elementName'] == 'Wx':
+                        forecast_dict[new_time]['condition_code'] = j['parameter'][0]['parameterValue']
+
+                    if i['elementName'] == 'Wind':
+                        for _ in j['parameter']:
+                            if _['parameterName'] == u'風向縮寫':
+                                forecast_dict[new_time]['wind_direction'] = _['parameterValue']
+                            elif _['parameterName'] == u'風速':
+                                forecast_dict[new_time]['wind_speed'] = _['parameterValue']
+                    elif i['elementName'] == 'UVI':
+                        for _ in j['parameter']:
+                            if _['parameterName'] == u'紫外線指數':
+                                forecast_dict[new_time]['UVI'] = _['parameterValue']
+                    else:
+                        forecast_dict[new_time][i['elementName']] = j['elementValue']
 
         forecast_list = []
         for forecast in sorted(forecast_dict.items()):
