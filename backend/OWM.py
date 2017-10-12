c10f41803d0ff5de322a305b6c8886674671690b
yan <a0726h77@gmail.com>
2017-10-12 18:35:46 +0800

    rename

--- backend/OWM.py
+++ backend/OWM.py
@@ -13,7 +13,7 @@ from utils import remap_dict_columns
 
 
 class OWM:
-    realtime_column_map = {
+    current_column_map = {
         "main": "condition",
         "temp": "temperature_c",
         "humidity": "humidity",
@@ -27,24 +27,24 @@ class OWM:
 
     # def get_realtime(self, id):
     def get_realtime(self, lat, lng):
-        realtime = {}
+        current = {}
 
         # url = 'http://api.openweathermap.org/data/2.5/weather?id=%s&APPID=%s' % (str(id), self.API_KEY)
         url = 'http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&APPID=%s' % (str(lat), str(lng), self.API_KEY)
 
         r = requests.get(url)
 
-        realtime.update(remap_dict_columns(r.json()['weather'][0], self.realtime_column_map, drop=True))
-        realtime.update(remap_dict_columns(r.json()['main'], self.realtime_column_map, drop=True))
-        realtime.update(remap_dict_columns(r.json()['sys'], self.realtime_column_map, drop=True))
-        realtime.update(r.json()['coord'])
-        realtime.update({'id': r.json()['id']})
-        realtime.update({'city': r.json()['name']})
-        realtime.update({'url': url})
+        current.update(remap_dict_columns(r.json()['weather'][0], self.current_column_map, drop=True))
+        current.update(remap_dict_columns(r.json()['main'], self.current_column_map, drop=True))
+        current.update(remap_dict_columns(r.json()['sys'], self.current_column_map, drop=True))
+        current.update(r.json()['coord'])
+        current.update({'id': r.json()['id']})
+        current.update({'city': r.json()['name']})
+        current.update({'url': url})
 
-        realtime['temperature_c'] = round(pytemperature.k2c(realtime['temperature_c']), 2)
+        current['temperature_c'] = round(pytemperature.k2c(current['temperature_c']), 2)
 
-        return json.dumps([realtime])
+        return current
 
     def get_forecast(self, dataset_id, town_index):
         pass
