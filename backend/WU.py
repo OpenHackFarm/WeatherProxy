2ded10c00c83fb70937e82d02ab7662071f44d71
yan <a0726h77@gmail.com>
2017-10-12 18:29:30 +0800

    Add Weather Underground backend

---
+++ backend/WU.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''
+https://www.wunderground.com/weather/api/d/docs
+'''
+
+import requests
+
+from utils import remap_dict_columns
+
+
+class WU:
+    realtime_column_map = {
+        "latitude": "latitude",
+        "longitude": "longitude",
+        "city": "city",
+        "temp_c": "temperature_c",
+        "relative_humidity": "humidity",
+        "pressure_mb": "pressure",
+        "observation_time_rfc822": "datetime",
+        "wind_dir": "wind_direction",
+        "wind_kph": "wind_speed"
+    }
+
+    def __init__(self, API_KEY):
+        self.API_KEY = API_KEY
+        self.url_base = 'http://api.wunderground.com/api/%s/conditions/forecast10day/q/' % self.API_KEY
+
+    # def get_realtime(self, id):
+    def get_realtime(self, lat, lng):
+        current = {}
+
+        url = self.url_base + '%s,%s.json' % (lat, lng)
+
+        r = requests.get(url)
+        r = r.json()['current_observation']
+        # return r
+
+        current.update(remap_dict_columns(r, self.realtime_column_map, drop=True))
+        current.update(remap_dict_columns(r['observation_location'], self.realtime_column_map, drop=True))
+
+        current['wind_speed'] = current['wind_speed'] * 0.27777777777778  # kph to mps
+        return current
+
+    def get_forecast(self, lat, lng):
+        forecast = {}
+
+        url = self.url_base + '%s,%s.json' % (lat, lng)
+
+        r = requests.get(url)
+        r = r.json()['forecast']
+        return r
