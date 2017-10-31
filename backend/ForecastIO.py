ea7677b957ce8f67e7fa6b49130be58731b791a4
yan <a0726h77@gmail.com>
2017-10-31 10:55:02 +0800

    Implement ForecastIO get_current

--- backend/ForecastIO.py
+++ backend/ForecastIO.py
@@ -1,11 +1,51 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+'''
+https://darksky.net/dev/docs
+
+https://github.com/damian-w/forecast.io-javascript-jsonp-api/blob/master/src/main.js
+'''
+
+import requests
+from datetime import datetime
+
+import pytemperature
+from utils import remap_dict_columns
+
 
 class ForecastIO:
-    @property
-    def get_current(self):
-        return "Forecast.io Realtime data."
+    current_column_map = {
+        "temperature": "temperature_c",
+        "summary": "condition",
+        "windSpeed": "wind_speed",
+        "windBearing": "wind_degrees",
+        "humidity": "humidity",
+        "pressure": "pressure",
+        "visibility": "visibility",
+        "time": "datetime",
+    }
+
+    def __init__(self, API_KEY):
+        self.API_KEY = API_KEY
+        self.url_base = 'https://api.darksky.net/forecast/%s/' % self.API_KEY
+
+    def get_current(self, lat, lng):
+        current = {}
+
+        url = self.url_base + '%s,%s' % (lat, lng)
+
+        r = requests.get(url)
+        r = r.json()
+
+        current.update(remap_dict_columns(r['currently'], self.current_column_map, drop=True))
+        current.update({'url': url})
+
+        current['temperature_c'] = round(pytemperature.f2c(current['temperature_c']), 2)
+        current['humidity'] = current['humidity'] * 100
+        current['datetime'] = datetime.fromtimestamp(current['datetime']).strftime("%Y-%m-%d %H:%M:%S")
+
+        return current
 
     @property
     def get_forecast(self):
