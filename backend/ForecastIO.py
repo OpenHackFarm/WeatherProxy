05a731e8703f296854539417a4de34181f841a06
yan <a0726h77@gmail.com>
2017-10-31 20:28:08 +0800

    Implement ForecastIO get_forecast

--- backend/ForecastIO.py
+++ backend/ForecastIO.py
@@ -26,6 +26,16 @@ class ForecastIO:
         "time": "datetime",
     }
 
+    forecast_column_map = {
+        "precipProbability": "PoP",
+        "humidity": "humidity",
+        "temperatureMin": "min_temperature_c",
+        "temperatureMax": "max_temperature_c",
+        "icon": "condition",
+        "summary": "description",
+        "time": "datetime"
+    }
+
     def __init__(self, API_KEY):
         self.API_KEY = API_KEY
         self.url_base = 'https://api.darksky.net/forecast/%s/' % self.API_KEY
@@ -47,6 +57,27 @@ class ForecastIO:
 
         return current
 
-    @property
-    def get_forecast(self):
-        return "Forecast.io Forecast data."
+    def get_forecast(self, lat, lng):
+        forecast = []
+
+        url = self.url_base + '%s,%s' % (lat, lng)
+
+        r = requests.get(url)
+        # return r.json()
+        r = r.json()['daily']['data']
+        # return r
+
+        for f in r:
+            forecast_dict = {}
+
+            forecast_dict.update(remap_dict_columns(f, self.forecast_column_map, drop=True))
+
+            forecast_dict['min_temperature_c'] = round(pytemperature.f2c(forecast_dict['min_temperature_c']), 2)
+            forecast_dict['max_temperature_c'] = round(pytemperature.f2c(forecast_dict['max_temperature_c']), 2)
+            forecast_dict['humidity'] = forecast_dict['humidity'] * 100
+            forecast_dict['PoP'] = forecast_dict['PoP'] * 100
+            forecast_dict['datetime'] = datetime.fromtimestamp(forecast_dict['datetime']).strftime("%Y-%m-%d %H:%M:%S")
+
+            forecast.append(forecast_dict)
+
+        return forecast
